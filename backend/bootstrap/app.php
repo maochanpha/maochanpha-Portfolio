8f404dd00e54bfa6311d38d7b8b40b023d832f40
maochanpha <maochanpha@gmail.com>
2026-07-10 16:18:28 +0700

    force api json error responses

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -3,7 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withProviders([
@@ -20,6 +22,15 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(function (Request $request, \Throwable $e) {
-            return $request->is('api/*') || $request->expectsJson();
+            return true;
+        });
+
+        $exceptions->render(function (\Throwable $e, Request $request) {
+            $status = $e instanceof HttpExceptionInterface ? $e->getStatusCode() : 500;
+
+            return new JsonResponse([
+                'message' => config('app.debug') ? $e->getMessage() : 'Server Error',
+                'exception' => config('app.debug') ? get_class($e) : null,
+            ], $status);
         });
     })->create();
