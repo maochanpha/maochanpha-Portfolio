bd4945d17694023739dc9ef1598430315fb7527b
maochanpha <maochanpha@gmail.com>
2026-07-10 14:00:53 +0700

    fix duplicate laravel start definition

--- backend/api/index.php
+++ backend/api/index.php
@@ -25,4 +25,4 @@ $_SERVER['SCRIPT_NAME'] = '/index.php';
 $_SERVER['SCRIPT_FILENAME'] = __DIR__.'/../public/index.php';
 $_SERVER['PHP_SELF'] = '/index.php';
 
-require __DIR__.'/../public/index.php';
+require_once __DIR__ . '/../public/index.php';
