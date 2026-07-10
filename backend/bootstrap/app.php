0ec80812a9c442457bc855cceed2ed2c2b028405
maochanpha <maochanpha@gmail.com>
2026-07-10 15:46:34 +0700

    force register laravel view provider

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -6,6 +6,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withProviders([
+        Illuminate\View\ViewServiceProvider::class,
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
